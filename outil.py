2b09922c7b182478e3a676d91e9aa403754a213f
Julie-Anne <101122818+Julie-Anne97@users.noreply.github.com>
2023-02-20 17:53:11 +0100

    Update outil.py

--- outil.py
+++ outil.py
@@ -170,6 +170,7 @@ if option == 'Option 1 : ∆tw - Calculate water flow from given delta T':
         st.title("")
         st.title("")
         st.title("")
+        st.markdown("")
         st.write("")
         st.markdown('**Heating inputs**')
         troom_heating = st.number_input('Reference Air Temperature - troom (°C)  ',value=20.0,step=0.1,)
